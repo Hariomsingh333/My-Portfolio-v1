6f334536673bec58c749b041a01fa385520335b8
Hari Om Singh <www.hariom03.com@gmail.com>
2021-06-10 17:13:02 +0530

    only routering are left

--- src/App.js
+++ src/App.js
@@ -6,21 +6,37 @@ import About from "./components/About";
 import Skill from "./components/Skill";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
+
+// react router dom
+
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Header />
-      <HeaderCard />
-      <hr />
-      <About />
-      <hr />
-      <Skill />
-      <hr />
-      <Project />
-      <hr />
-      <Contact />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Navbar />
+            <Header />
+            <HeaderCard />
+            <hr />
+          </Route>
+
+          <Route exact path="/About">
+            {/* <About /> */}
+            <About />
+            <hr />
+          </Route>
+          <Skill />
+          <hr />
+          <Project />
+          <hr />
+          <Contact />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
